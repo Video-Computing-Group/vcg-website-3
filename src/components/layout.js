666c736ae5dfc524c0aedc4486ca50cc97d4971d
John Pham <johnphammail@gmail.com>
2019-02-19 13:34:29 -0800

    adding styles for body

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,21 +9,14 @@ const Layout = ({ children }) => (
   <LayoutContainer>
     <Wrapper>
       <Header />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0
-        }}
-      >
-        <main>{children}</main>
+      <>
+        <ContentWrapper>{children}</ContentWrapper>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </>
     </Wrapper>
   </LayoutContainer>
 );
@@ -44,4 +37,9 @@ const Wrapper = styled.div`
 const LayoutContainer = styled.div`
   background: ${background};
   min-height: 100vh;
+`;
+
+const ContentWrapper = styled.main`
+  background: white;
+  padding: 30px 5vw;
 `;
